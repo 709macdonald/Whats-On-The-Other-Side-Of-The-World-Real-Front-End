b9c196f73816163dd885d2cf22bdb96a4c219a55
709macdonald <709macdonald@gmail.com>
2025-04-29 18:11:30 -0230

    reorder navigation buttons

--- src/components/Footer/NavigationButtons.jsx
+++ src/components/Footer/NavigationButtons.jsx
@@ -16,12 +16,9 @@ function NavigationButtons({
 
   return (
     <div className="button-container">
-      {searchCount > 0 && (
-        <button className="btn btn-blue" onClick={onReset}>
-          Search Again ({adjustedSearchCount >= 0 ? adjustedSearchCount : 0}{" "}
-          Left)
-        </button>
-      )}
+      <button className="btn btn-yellow" onClick={onViewMcDonalds}>
+        Nearest McDonald's
+      </button>
 
       <button className="btn btn-green" onClick={onViewOriginal}>
         My Location
@@ -31,9 +28,12 @@ function NavigationButtons({
         Other Side Of The World
       </button>
 
-      <button className="btn btn-yellow" onClick={onViewMcDonalds}>
-        Nearest McDonald's
-      </button>
+      {searchCount > 0 && (
+        <button className="btn btn-blue" onClick={onReset}>
+          Search Again ({adjustedSearchCount >= 0 ? adjustedSearchCount : 0}{" "}
+          Left)
+        </button>
+      )}
     </div>
   );
 }
